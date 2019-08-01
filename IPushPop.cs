1f7fa87e9085e6c09906e7f15de912eb91362ed5
mwacaser <michael@wacaser.org>
2019-08-01 05:46:08 -0700

    Initial commit

---
+++ IPushPop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public interface IPushPop<T> : ICollection<T> {
+    T Pop();
+    T Peek();
+    bool hasNext { get; }
+}
+
+public class PPQueue<T> : Queue<T>, IPushPop<T> {
+    public bool hasNext => Count > 0;
+    public bool IsReadOnly => false;
+
+    public void Add(T item) => Enqueue(item);
+    public T Pop() => Dequeue();
+    public bool Remove(T item) => false;
+}
+
+public class PPStack<T> : Stack<T>, IPushPop<T> {
+    public bool hasNext => Count > 0;
+    public bool IsReadOnly => false;
+
+    public void Add(T item) => Push(item);
+    public bool Remove(T item) => false;
+}
